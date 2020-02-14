5065b3f1b65991f5d7ca6025a92ddcabd6ac699a
ZhangHui423 <1980282864@qq.com>
2020-02-14 13:20:10 +0800

    瀑布流

--- miniprogram/pages/logs/logs.js
+++ miniprogram/pages/logs/logs.js
@@ -1,21 +1,30 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var util_1 = require("../../utils/util");
-var img=require("../../utils/img.js")
+//index.js
+//获取应用实例
+const imgs = require('../../utils/img')
+const app = getApp()
+
+
 Page({
-    data: {
-        logs: [],
-        img:[]
-        
-    },
-    onLoad: function () {
-        
-        this.setData({
-            logs: (wx.getStorageSync('logs') || []).map(function (log) {
-                return util_1.formatTime(new Date(log));
-            }),
-            img:img
-        });
-    },
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibG9ncy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImxvZ3MudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFQSx5Q0FBNkM7QUFFN0MsSUFBSSxDQUFDO0lBQ0gsSUFBSSxFQUFFO1FBQ0osSUFBSSxFQUFFLEVBQUU7S0FDVDtJQUNELE1BQU07UUFDSixJQUFJLENBQUMsT0FBTyxDQUFDO1lBQ1gsSUFBSSxFQUFFLENBQUMsRUFBRSxDQUFDLGNBQWMsQ0FBQyxNQUFNLENBQUMsSUFBSSxFQUFFLENBQUMsQ0FBQyxHQUFHLENBQUMsVUFBQyxHQUFXO2dCQUN0RCxPQUFPLGlCQUFVLENBQUMsSUFBSSxJQUFJLENBQUMsR0FBRyxDQUFDLENBQUMsQ0FBQTtZQUNsQyxDQUFDLENBQUM7U0FDSCxDQUFDLENBQUE7SUFDSixDQUFDO0NBQ0YsQ0FBQyxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiLy8gbG9ncy50c1xuLy8gY29uc3QgdXRpbCA9IHJlcXVpcmUoJy4uLy4uL3V0aWxzL3V0aWwuanMnKVxuaW1wb3J0IHsgZm9ybWF0VGltZSB9IGZyb20gJy4uLy4uL3V0aWxzL3V0aWwnXG5cblBhZ2Uoe1xuICBkYXRhOiB7XG4gICAgbG9nczogW10sXG4gIH0sXG4gIG9uTG9hZCgpIHtcbiAgICB0aGlzLnNldERhdGEoe1xuICAgICAgbG9nczogKHd4LmdldFN0b3JhZ2VTeW5jKCdsb2dzJykgfHwgW10pLm1hcCgobG9nOiBzdHJpbmcpID0+IHtcbiAgICAgICAgcmV0dXJuIGZvcm1hdFRpbWUobmV3IERhdGUobG9nKSlcbiAgICAgIH0pLFxuICAgIH0pXG4gIH0sXG59KVxuIl19
+  data: {
+    columns: []
+  },
+  onShow() {
+
+    this.setData({
+      columns: [imgs.slice(0, 30), imgs.slice(30, 60), imgs.slice(60, 90)]
+    })
+  },
+  onPullDownRefresh() {
+    console.log('用户触发了下拉操作....');
+  },
+  onReachBottom() {
+    console.log('用户触发了上拉加载操作.....');
+  },
+  onShareAppMessage(info) {
+    return {
+      title: '我的页面',
+      path: '/pages/logs/logs',
+      imageUrl: ''
+    }
+  }
+})
